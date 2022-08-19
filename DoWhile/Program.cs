f1d1f4d4daba0b577984f0b578465ca60d02905b
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-18 23:04:59 -0700

    Do while loop

---
+++ DoWhile/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite color:");
+            string colorGuessed = Console.ReadLine();
+            bool favColor = colorGuessed == "green";
+
+
+            do
+            {
+                switch (colorGuessed)
+                {
+                    case "red":
+                        Console.WriteLine("You guessed red. Try again.");
+                        Console.WriteLine("Guess my favorite color:");
+                        colorGuessed = Console.ReadLine();
+                        break;
+
+                    case "blue":
+                        Console.WriteLine("You guessed Blue. Try again, sorry.");
+                        Console.WriteLine("Guess my favorite color:");
+                        colorGuessed = Console.ReadLine();
+                        break;
+
+                    case "pink":
+                        Console.WriteLine("You guessed Pink. Try again.");
+                        Console.WriteLine("Guess my favorite color:");
+                        colorGuessed = Console.ReadLine();
+                        break;
+
+                    case "green":
+                        Console.WriteLine("You guessed green! That's correct!");
+                        favColor = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Nope. Try again.");
+                        Console.WriteLine("Guess my favorite color:");
+                        colorGuessed = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!favColor);
+
+            Console.Read();
+        }
+    }
+}
