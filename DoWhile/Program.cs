9c658fbb45245e2929f58491a208618ed5d9384b
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-18 23:08:38 -0700

    Update Program.cs

--- DoWhile/Program.cs
+++ DoWhile/Program.cs
@@ -10,15 +10,17 @@ namespace DoWhile
     {
         static void Main(string[] args)
         {
+            //asks to guess favorite color and assigns my favorite color green
             Console.WriteLine("Guess my favorite color:");
             string colorGuessed = Console.ReadLine();
             bool favColor = colorGuessed == "green";
 
 
-            do
+            do //stops infinite loops created by while loop
             {
-                switch (colorGuessed)
+                switch (colorGuessed) //switch statement that stops repeat branching statements
                 {
+                    //tells users red, blue and pink are the wrong colors
                     case "red":
                         Console.WriteLine("You guessed red. Try again.");
                         Console.WriteLine("Guess my favorite color:");
@@ -37,11 +39,13 @@ namespace DoWhile
                         colorGuessed = Console.ReadLine();
                         break;
 
+                    //lets user know green is correct color
                     case "green":
                         Console.WriteLine("You guessed green! That's correct!");
                         favColor = true;
                         break;
 
+                    //lets user know any guesses outside of red blue pink and green are wrong
                     default:
                         Console.WriteLine("Nope. Try again.");
                         Console.WriteLine("Guess my favorite color:");
