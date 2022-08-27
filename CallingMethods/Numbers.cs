7eb80ff36a1de3ed2e71fcf685e81ea58b0cdc93
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-26 18:04:22 -0700

    Calling methods created

---
+++ CallingMethods/Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Numbers
+    {
+        //creates public method for addition
+        public int Add(int i)
+        {
+            int total = i + 20;
+            return total;
+        }
+
+        //creates public method for division
+        public int Divide(int j)
+        {
+            int total = j / 2;
+            return total;
+        }
+
+        //creates public method for multiplication
+        public int Multiply(int k)
+        {
+            int total = k * 4;
+            return total;
+
+        }
+    }
+}
--- CallingMethods/Program.cs
+++ CallingMethods/Program.cs
@@ -10,6 +10,27 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
+            //Asks user to put in a number
+            Console.WriteLine("Type your favorite number:");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //creates var
+            Numbers number = new Numbers();
+            
+            //uses addition method to add to users favorite number
+            int total = number.Add(favNum);
+            Console.WriteLine("20 plus " + favNum + " = " + total);
+
+            //uses division method to add to users favorite number
+            total = number.Divide(favNum);
+            Console.WriteLine( favNum + " divided by 2 " + " = " + total);
+
+            //uses multiplication method to add to users favorite number
+            total = number.Multiply(favNum);
+            Console.WriteLine("4 times " + favNum + " = " + total);
+
+
+            Console.ReadLine();
         }
     }
 }
