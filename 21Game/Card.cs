ae4e97938ff3773f09549787814c1453f1de3ac2
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-26 17:20:48 -0700

    21 game

---
+++ 21Game/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21Game
+{
+    public class Card
+    {
+        //a constructor
+        //public Card()
+        //{
+        //    Suit = "Spades";
+        //    Face = "Two";
+        //}
+        //gives a property get and set all they do methods are for things you can do
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
