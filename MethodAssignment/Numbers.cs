59c109acaa95a737eccc051d08e14345c6d841a2
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-28 00:33:53 -0700

    Method assignment

---
+++ MethodAssignment/Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; //added to use optional feature c-sharpcorner.com
+
+namespace MethodAssignment
+{
+    public class Numbers
+    {
+        //creates method for 2 integers to be added 1 being option (j)
+        public int Add(int i, [Optional] int j)
+        {
+            int k = i + j;
+            return k;
+        }
+
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uses class Numbers to create number object
+            Numbers number = new Numbers();
+
+            //asks user for first number
+            Console.WriteLine("Input a number:");
+            int l = Convert.ToInt32(Console.ReadLine());
+
+            //Asks user for second number and creates var m
+            int m;
+            Console.WriteLine("Input a second number: (Optional)");
+            //int m = Convert.ToInt32(Console.ReadLine());
+
+            //creates variable total to output
+            int total;
+            
+            if (int.TryParse(Console.ReadLine(), out m))
+                total = number.Add(m);
+
+            Console.WriteLine(number.Add(l, m));
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
