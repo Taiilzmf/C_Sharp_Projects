d5c69459ac13f529ad57bbed5e71683470beffc9
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-29 12:40:53 -0700

    finished method class

--- MethodClass/Program.cs
+++ MethodClass/Program.cs
@@ -10,7 +10,16 @@ namespace MethodClass
     {
         static void Main(string[] args)
         {
+            //calls class
+            Numbers number = new Numbers();
 
+            //creates vars 
+            int i = 7;
+            int j = 22;
+
+            //Passes in 2 numbers to method Add and then calls by name
+            Numbers.Add(7, 22);
+            Numbers.Add(i, j);
         }
     }
 }
