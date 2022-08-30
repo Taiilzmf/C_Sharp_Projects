f3d365142c8dc346e432cb1208c88ed069b1df60
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-30 14:47:49 -0700

    Constructor assignment

---
+++ ConstructorSub/Multiverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSub
+{
+    public class Multiverse
+    {
+        public int Wins { get; set; }
+        public string Username {get; set;}
+
+        //creates class with my username and wins in Multiverse
+
+        public Multiverse(string username, int wins)
+        {
+            Username = username;
+            Wins = wins;
+        }
+
+        public Multiverse(string username) : this (username, 27)
+        {
+
+        }
+    }
+}
---
+++ ConstructorSub/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sets constant string
+            const string multiName = "Taiilzmf";
+
+            //const string sets var using chained constructor
+            var multi = new Multiverse(multiName);
+
+            //Write Taiilzmf has 27 wins.
+            Console.WriteLine("{0} has {1} wins.", multi.Username, multi.Wins);
+
+
+            Console.ReadLine();
+        }
+    }
+}
