b0499475e3d5db598f5c7261001cd1d3e5f57e1d
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-29 18:47:16 -0700

    creates operators assignment

---
+++ Operators/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+
+        //overload operator
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            //compares Id to make sure both employees are the same
+            return employee1.Id == employee2.Id;
+        }
+        //creates pair for == with not equal to
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            //compares not equal 2
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciates
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            //gives emplyees 2 different ids
+            employee1.Id = "27";
+            employee2.Id = "15";
+
+            //prints false
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
