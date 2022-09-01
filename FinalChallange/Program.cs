9747a43d645ec6654ca8711df23a49baf3e8355e
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-08-31 22:26:53 -0700

    created final challenge

---
+++ FinalChallange/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+namespace FinalChallange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new StudentContext())
+            {
+                var stud = new Student() { FirstName = "Shaelynn" };
+
+                ctx.Students?.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+
+   
+
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
